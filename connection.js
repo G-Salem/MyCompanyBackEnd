89bd77d77d6dd211aa97f8ca42822ab0147f8872
Mohamed Salem Gabsi <gabsi.salem1@esprit.tn>
2022-02-16 11:12:48 +0100

    commit 2

--- connection.js
+++ connection.js
@@ -1,10 +1,20 @@
 const Pool = require('pg').Pool
+    // local //
+// const pool = new Pool({
+//     host: "localhost",
+//     user: "postgres",
+//     port: 5432,
+//     password: "admin",
+//     database: "Logimes"
+// })
 
+    // Server //
+    
 const pool = new Pool({
     host: "localhost",
     user: "postgres",
     port: 5432,
-    password: "admin",
+    password: "Logime$2022",
     database: "Logimes"
 })
 
